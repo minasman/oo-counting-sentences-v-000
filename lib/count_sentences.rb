835ae6011e19eb0baf2c503b0e5cc4acf519432e
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-24 11:14:50 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,8 +17,6 @@ class String
   def count_sentences
     sentence = self.split(/[?.!]/)
     num = sentence.count {|word| word.length > 0}
-    binding.pry 
   end
 end
 
-"HI? How Are You? I am Fine!!! Woo...".count_sentences
